257f3b9f35d1e152579816568f0b7403f2498f48
twomack <tylerjswomack@gmail.com>
2018-05-04 16:27:22 -0400

    Added more stars

--- app/src/main/java/com/example/twomack/animationtest/FlingAnimationActivity.java
+++ app/src/main/java/com/example/twomack/animationtest/FlingAnimationActivity.java
@@ -14,7 +14,13 @@ import android.widget.ImageView;
 public class FlingAnimationActivity extends AppCompatActivity {
 
     ImageView star;
-    private GestureDetector mDetector;
+    ImageView star2;
+    ImageView star3;
+    ImageView star4;
+    private GestureDetector mDetector1;
+    private GestureDetector mDetector2;
+    private GestureDetector mDetector3;
+    private GestureDetector mDetector4;
     float height;
     float width;
 
@@ -30,54 +36,86 @@ public class FlingAnimationActivity extends AppCompatActivity {
         height = size.y;
 
         star = findViewById(R.id.star);
-        mDetector = new GestureDetector(this, new MyGestureListener());
-        star.setOnTouchListener(new View.OnTouchListener() {
+        star2 = findViewById(R.id.star2);
+        star3 = findViewById(R.id.star3);
+        star4 = findViewById(R.id.star4);
+
+        mDetector1 = new GestureDetector(this, new MyGestureListener(star));
+        mDetector2 = new GestureDetector(this, new MyGestureListener(star2));
+        mDetector3 = new GestureDetector(this, new MyGestureListener(star3));
+        mDetector4 = new GestureDetector(this, new MyGestureListener(star4));
+
+        setGestureDetector(mDetector1, star);
+        setGestureDetector(mDetector2, star2);
+        setGestureDetector(mDetector3, star3);
+        setGestureDetector(mDetector4, star4);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public void setGestureDetector(final GestureDetector gestureDetector, ImageView imageView){
+        imageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                mDetector.onTouchEvent(event);
+                gestureDetector.onTouchEvent(event);
                 return true;
             }
         });
     }
 
+
+
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
+
+        ImageView imageView;
+
+        public MyGestureListener(ImageView imageView){
+            this.imageView = imageView;
+        }
+
         @Override
         public boolean onFling(MotionEvent event1, MotionEvent event2,
                                float velocityX, float velocityY) {
 
-            final FlingAnimation flingX = new FlingAnimation(star, DynamicAnimation.X);
-            flingX.setStartVelocity(velocityX)
-                    .setFriction(.5f)
-                    .addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
-                        @Override
-                        public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
-                                if (star.getX() < 0 && velocity < 0){
-                                    flingX.setStartVelocity(0 - velocity);
-                                }else if(star.getX() > width - ((star.getWidth() - star.getPaddingRight())) && velocity > 0){
-                                    flingX.setStartVelocity(0 - velocity);
-                                }
-                                flingX.start();
-                        }
-                    })
-                    .start();
+            flingX(imageView, velocityX);
+            flingY(imageView, velocityY);
 
-            final FlingAnimation flingY = new FlingAnimation(star, DynamicAnimation.Y);
-            flingY.setStartVelocity(velocityY)
-                    .setFriction(.5f)
-                    .addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
-                        @Override
-                        public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
-
-                                if (star.getY() < 0 && velocity < 0){
-                                    flingY.setStartVelocity(0 - velocity);
-                                }else if(star.getY() > height - ((star.getHeight() * 2)) && velocity > 0){
-                                    flingY.setStartVelocity(0 - velocity);
-                                }
-                                flingY.start();
-                        }
-                    })
-                    .start();
             return true;
         }
+    }
+
+    public void flingX(final ImageView object,
+                       float velocityX){
+        final FlingAnimation flingX = new FlingAnimation(object, DynamicAnimation.X);
+        flingX.setStartVelocity(velocityX)
+                .setFriction(.5f)
+                .addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
+                        if (object.getX() < 0 && velocity < 0){
+                            flingX.setStartVelocity(0 - velocity);
+                        }else if(object.getX() > width - ((object.getWidth() - object.getPaddingRight())) && velocity > 0){
+                            flingX.setStartVelocity(0 - velocity);
+                        }
+                    }
+                })
+                .start();
+    }
+
+    public void flingY(final ImageView object, float velocityY){
+        final FlingAnimation flingY = new FlingAnimation(object, DynamicAnimation.Y);
+        flingY.setStartVelocity(velocityY)
+                .setFriction(.5f)
+                .addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
+
+                        if (object.getY() < 0 && velocity < 0){
+                            flingY.setStartVelocity(0 - velocity);
+                        }else if(object.getY() > height - ((object.getHeight() * 2)) && velocity > 0){
+                            flingY.setStartVelocity(0 - velocity);
+                        }
+                    }
+                })
+                .start();
     }
 }
