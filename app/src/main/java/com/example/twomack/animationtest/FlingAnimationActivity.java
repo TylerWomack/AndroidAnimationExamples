0fc563fd47ad4c56f1f309d98def11c0afc616e2
twomack <tylerjswomack@gmail.com>
2018-05-07 16:41:03 -0400

    Made the stars break apart when they hit the walls.

--- app/src/main/java/com/example/twomack/animationtest/FlingAnimationActivity.java
+++ app/src/main/java/com/example/twomack/animationtest/FlingAnimationActivity.java
@@ -2,27 +2,27 @@ package com.example.twomack.animationtest;
 
 import android.annotation.SuppressLint;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.animation.DynamicAnimation;
 import android.support.animation.FlingAnimation;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class FlingAnimationActivity extends AppCompatActivity {
 
-    ImageView star;
-    ImageView star2;
-    ImageView star3;
-    ImageView star4;
-    private GestureDetector mDetector1;
-    private GestureDetector mDetector2;
-    private GestureDetector mDetector3;
-    private GestureDetector mDetector4;
     float height;
     float width;
+    float friction = .7f;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -35,20 +35,7 @@ public class FlingAnimationActivity extends AppCompatActivity {
         width = size.x;
         height = size.y;
 
-        star = findViewById(R.id.star);
-        star2 = findViewById(R.id.star2);
-        star3 = findViewById(R.id.star3);
-        star4 = findViewById(R.id.star4);
-
-        mDetector1 = new GestureDetector(this, new MyGestureListener(star));
-        mDetector2 = new GestureDetector(this, new MyGestureListener(star2));
-        mDetector3 = new GestureDetector(this, new MyGestureListener(star3));
-        mDetector4 = new GestureDetector(this, new MyGestureListener(star4));
-
-        setGestureDetector(mDetector1, star);
-        setGestureDetector(mDetector2, star2);
-        setGestureDetector(mDetector3, star3);
-        setGestureDetector(mDetector4, star4);
+        createNewStar(0, 0, 0, 0, null);
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -62,6 +49,41 @@ public class FlingAnimationActivity extends AppCompatActivity {
         });
     }
 
+    public void createNewStar(float x, float y, @Nullable float velocityY, @Nullable float velocityX, @Nullable ImageView oldStar){
+        ImageView newStar = new ImageView(this);
+        newStar.setImageDrawable(getResources().getDrawable(android.R.drawable.btn_star_big_on));
+
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
+
+        if (oldStar == null){
+            ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(800, 800);
+            constraintLayout.addView(newStar, -1, lp);
+            newStar.setX(x);
+            newStar.setY(y);
+            GestureDetector newDetector = new GestureDetector(this, new MyGestureListener(newStar));
+            setGestureDetector(newDetector, newStar);
+            return;
+        }
+
+        ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(oldStar.getHeight()/2, oldStar.getWidth()/2);
+        constraintLayout.addView(newStar, -1, lp);
+
+        oldStar.setLayoutParams(lp);
+
+        newStar.setX(x);
+        newStar.setY(y);
+
+        GestureDetector newDetector = new GestureDetector(this, new MyGestureListener(newStar));
+        setGestureDetector(newDetector, newStar);
+
+        if (velocityX != 0) {
+            setInitialX(newStar, velocityX);
+        }
+
+        if (velocityY!= 0){
+            setInitialY(newStar, velocityY);
+        }
+    }
 
 
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
@@ -84,35 +106,80 @@ public class FlingAnimationActivity extends AppCompatActivity {
     }
 
     public void flingX(final ImageView object,
-                       float velocityX){
+                       final float velocityX){
         final FlingAnimation flingX = new FlingAnimation(object, DynamicAnimation.X);
         flingX.setStartVelocity(velocityX)
-                .setFriction(.5f)
+                .setFriction(friction)
                 .addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
                     @Override
                     public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
+
+                        float newVelocity = 0 - velocity;
+
                         if (object.getX() < 0 && velocity < 0){
-                            flingX.setStartVelocity(0 - velocity);
+                            flingX.setStartVelocity(newVelocity);
+                            createNewStar(object.getX(), object.getY(), 0, velocity, object);
                         }else if(object.getX() > width - ((object.getWidth() - object.getPaddingRight())) && velocity > 0){
-                            flingX.setStartVelocity(0 - velocity);
+                            flingX.setStartVelocity(newVelocity);
+                            createNewStar(object.getX(), object.getY(), 0, velocity, object);
                         }
                     }
                 })
                 .start();
     }
 
-    public void flingY(final ImageView object, float velocityY){
+    public void flingY(final ImageView object, final float velocityY){
         final FlingAnimation flingY = new FlingAnimation(object, DynamicAnimation.Y);
         flingY.setStartVelocity(velocityY)
-                .setFriction(.5f)
+                .setFriction(friction)
                 .addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
                     @Override
                     public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
-
+                        float newVelocity = 0 - velocity;
                         if (object.getY() < 0 && velocity < 0){
-                            flingY.setStartVelocity(0 - velocity);
+                            flingY.setStartVelocity(newVelocity);
+                            createNewStar(object.getX(), object.getY(), velocity, 0, object);
                         }else if(object.getY() > height - ((object.getHeight() * 2)) && velocity > 0){
-                            flingY.setStartVelocity(0 - velocity);
+                            flingY.setStartVelocity(newVelocity);
+                            createNewStar(object.getX(), object.getY(), velocity, 0, object);
+                        }
+                    }
+                })
+                .start();
+    }
+
+    //same as a normal FlingX, except it doesn't splinter into new stars when it hits the walls.
+    public void setInitialX(final ImageView object, final float velocityX){
+        final FlingAnimation flingX = new FlingAnimation(object, DynamicAnimation.X);
+        flingX.setStartVelocity(velocityX)
+                .setFriction(friction)
+                .addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
+                        float newVelocity = 0 - velocity;
+                        if (object.getX() < 0 && velocity < 0){
+                            flingX.setStartVelocity(newVelocity);
+                        }else if(object.getX() > width - ((object.getWidth() - object.getPaddingRight())) && velocity > 0){
+                            flingX.setStartVelocity(newVelocity);
+                        }
+                    }
+                })
+                .start();
+    }
+
+    //same as a normal FlingY, except it doesn't splinter into new stars when it hits the walls.
+    public void setInitialY(final ImageView object, final float velocityY){
+        final FlingAnimation flingY = new FlingAnimation(object, DynamicAnimation.Y);
+        flingY.setStartVelocity(velocityY)
+                .setFriction(friction)
+                .addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
+                        float newVelocity = 0 - velocity;
+                        if (object.getY() < 0 && velocity < 0){
+                            flingY.setStartVelocity(newVelocity);
+                        }else if(object.getY() > height - ((object.getHeight() * 2)) && velocity > 0){
+                            flingY.setStartVelocity(newVelocity);
                         }
                     }
                 })
